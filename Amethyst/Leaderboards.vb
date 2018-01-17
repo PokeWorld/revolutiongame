4d9b2df0b00603be0393850271e528d197689247
TheEdge <trey6979@gmail.com>
2018-01-17 07:05:11 -0500

    Remove all VB files from master

--- Amethyst/Leaderboards.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class Leaderboards
-
-End Class
--- Amethyst/SiteAnalytics.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class SiteAnalytics
-    Private Sub tmData_Tick(sender As Object, e As EventArgs) Handles tmData.Tick
-        lblSiteName.Text = "Network Name: " & My.Settings.SiteName
-        lblAdIntensity.Text = "Advertising Intensity: " & My.Settings.AdIntensity.ToString()
-        lblUserCount.Text = "Users: " & My.Settings.Users.ToString()
-    End Sub
-End Class
