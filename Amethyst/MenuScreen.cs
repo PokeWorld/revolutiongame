f1137217aca75e6e19b1738d363edbc0f76477b2
TheEdge <trey6979@gmail.com>
2018-01-17 18:37:01 -0500

    Added changing main menu colors and changed some tip names

--- Amethyst/MenuScreen.cs
+++ Amethyst/MenuScreen.cs
@@ -29,33 +29,63 @@ namespace Amethyst
             {
                 case 10:
                     lblAdvice.Text = "I promise this will be finished by mid 2019";
+                    this.BackColor = System.Drawing.Color.Aqua;
+                    btnPlay.BackColor = System.Drawing.Color.Aqua;
+                    btnExit.BackColor = System.Drawing.Color.Aqua;
                     break;
                 case 9:
                     lblAdvice.Text = "Puzzled? Go to the 'Site Controller' application to get started.";
+                    this.BackColor = System.Drawing.Color.OrangeRed;
+                    btnPlay.BackColor = System.Drawing.Color.OrangeRed;
+                    btnExit.BackColor = System.Drawing.Color.OrangeRed;
                     break;
                 case 8:
-                    lblAdvice.Text = "Still in C#, and VERY CLOSED SOURCE.";
+                    lblAdvice.Text = "Now in C#, and still VERY CLOSED SOURCE.";
+                    this.BackColor = System.Drawing.Color.Crimson;
+                    btnPlay.BackColor = System.Drawing.Color.Crimson;
+                    btnExit.BackColor = System.Drawing.Color.Crimson;
                     break;
                 case 7:
                     lblAdvice.Text = "Hi! I am a bug. Just kidding.";
+                    this.BackColor = System.Drawing.Color.LimeGreen;
+                    btnPlay.BackColor = System.Drawing.Color.LimeGreen;
+                    btnExit.BackColor = System.Drawing.Color.LimeGreen;
                     break;
                 case 6:
                     lblAdvice.Text = "If Rylan was a developer, this would be Google Cod. :fish:";
+                    this.BackColor = System.Drawing.Color.Salmon;
+                    btnPlay.BackColor = System.Drawing.Color.Salmon;
+                    btnExit.BackColor = System.Drawing.Color.Salmon;
                     break;
                 case 5:
                     lblAdvice.Text = "Seriously, don't run this at ANYTHING higher than 1080p";
+                    this.BackColor = System.Drawing.Color.Black;
+                    btnPlay.BackColor = System.Drawing.Color.Black;
+                    btnExit.BackColor = System.Drawing.Color.Black;
                     break;
                 case 4:
                     lblAdvice.Text = "Does C# support Discord Rich Presence? If so, it will be here (eventually)";
+                    this.BackColor = System.Drawing.Color.CadetBlue;
+                    btnPlay.BackColor = System.Drawing.Color.CadetBlue;
+                    btnExit.BackColor = System.Drawing.Color.CadetBlue;
                     break;
                 case 3:
                     lblAdvice.Text = "bytes and bits";
+                    this.BackColor = System.Drawing.Color.DeepPink;
+                    btnPlay.BackColor = System.Drawing.Color.DeepPink;
+                    btnExit.BackColor = System.Drawing.Color.DeepPink;
                     break;
                 case 2:
                     lblAdvice.Text = "Tech Demo 1. (c) Pallet Games";
+                    this.BackColor = System.Drawing.Color.Teal;
+                    btnPlay.BackColor = System.Drawing.Color.Teal;
+                    btnExit.BackColor = System.Drawing.Color.Teal;
                     break;
                 default:
-                    lblAdvice.Text = "hi im out of ideas";
+                    lblAdvice.Text = "More gameplay than ForumLife";
+                    this.BackColor = System.Drawing.Color.DarkMagenta;
+                    btnPlay.BackColor = System.Drawing.Color.Purple;
+                    btnExit.BackColor = System.Drawing.Color.Purple;
                     break;
             }
         }
